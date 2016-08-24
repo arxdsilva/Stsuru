28ba58d11f99f26e7bc97f680aa9a6d0b9768203
Arthur Silva <arthur.001.002@gmail.com>
2016-08-24 19:00:12 -0300

    adds new gorilla, pongo2 & home rendering

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+
+	"github.com/flosch/pongo2"
+	"github.com/gorilla/mux"
 )
 
 type lines struct {
@@ -18,20 +18,23 @@ type lines struct {
 	Hash  string
 }
 
+var t = pongo2.Must(pongo2.FromFile("my.html"))
+
 func main() {
 	r := mux.NewRouter()
-	// iris.UseTemplate(django.New()).Directory("./templates", ".html")
-	r.HandleFunc("/", homer)
-	r.HandleFunc("/links", addLink)
-	r.HandleFunc("/links/remove/{id}", removeLink)
-	r.HandleFunc("/links/{id}", linkSolver)
+	r.HandleFunc("/", home)
 	http.Handle("/", r)
-	http.ListenAndServe("tsu.ru:8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
-func homer(w http.ResponseWriter, r *http.Request) {
-	data := []lines{}
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("NOT REAL")
+	_, err := t.Execute(pongo2.Context{})
+	if err != nil {
+		log.Panic(err)
+	}
 
+	data := []lines{}
 	session, err := mgo.Dial("localhost")
 	defer session.Close()
 	if err != nil {
@@ -46,63 +49,64 @@ func homer(w http.ResponseWriter, r *http.Request) {
 
 	context := map[string]interface{}{}
 	context["array"] = data
-	// ctx.Render("mypage.html", context)
 }
 
-func addLink(w http.ResponseWriter, r *http.Request) {
-	// link := ctx.FormValueString("user_link")
-	if link == "" {
-		// ctx.Redirect("/")
-	}
-
-	h := md5.New()
-	io.WriteString(h, link)
-	hash := string(h.Sum(nil))
-	linkshort := fmt.Sprintf("http://tsu.ru:8080/%x", hash)
-	dbHash := fmt.Sprintf("%x", hash)
-
-	linha := &lines{Link: link, Short: linkshort, Hash: dbHash}
-	session, err := mgo.Dial("localhost")
-	defer session.Close()
-	if err != nil {
-		log.Panic(err)
-	}
-	err = session.DB("tsuru").C("links").Insert(linha)
-	if err != nil {
-		log.Panic(err)
-	}
-	// ctx.Redirect("/")
-}
-
-func removeLink(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)
-	// id := ctx.Param("id")
-	session, err := mgo.Dial("localhost")
-	defer session.Close()
-	if err != nil {
-		log.Panic(err)
-	}
-	c := session.DB("tsuru").C("links")
-	err = c.Remove(bson.M{"hash": id})
-	if err != nil {
-		log.Panic(err)
-	}
-	// ctx.Redirect("/")
-}
-
-func linkSolver(w http.ResponseWriter, r *http.Request) {
-	// id := ctx.Param("id")
-	dbData := lines{}
-	id = fmt.Sprintf("%s", id)
-
-	session, err := mgo.Dial("localhost")
-	defer session.Close()
-	if err != nil {
-		log.Panic(err)
-	}
-	c := session.DB("tsuru").C("links").Find(bson.M{"hash": id}).One(&dbData)
-	if c != nil {
-		// ctx.Redirect("/")
-	}
-	// ctx.Redirect(dbData.Link)
-}
+// ____________________________________________________________________________________________
+//
+// func addLink(w http.ResponseWriter, r *http.Request) {
+// 	// link := ctx.FormValueString("user_link")
+// 	// if link == "" {
+// 	// 	// ctx.Redirect("/")
+// 	// }
+//
+// 	h := md5.New()
+// 	// io.WriteString(h, link)
+// 	hash := string(h.Sum(nil))
+// 	linkshort := fmt.Sprintf("http://tsu.ru:8080/%x", hash)
+// 	dbHash := fmt.Sprintf("%x", hash)
+//
+// 	// linha := &lines{Link: link, Short: linkshort, Hash: dbHash}
+// 	session, err := mgo.Dial("localhost")
+// 	defer session.Close()
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	err = session.DB("tsuru").C("links").Insert(linha)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	http.Redirect(w, r, "/", http.StatusFound)
+// }
+//
+// func removeLink(w http.ResponseWriter, r *http.Request) {
+// 	id := mux.Vars(r)
+// 	// id := ctx.Param("id")
+// 	session, err := mgo.Dial("localhost")
+// 	defer session.Close()
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	c := session.DB("tsuru").C("links")
+// 	err = c.Remove(bson.M{"hash": id})
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	// ctx.Redirect("/")
+// }
+//
+// func linkSolver(w http.ResponseWriter, r *http.Request) {
+// 	// id := ctx.Param("id")
+// 	dbData := lines{}
+// 	// id = fmt.Sprintf("%s", id)
+//
+// 	session, err := mgo.Dial("localhost")
+// 	defer session.Close()
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	c := session.DB("tsuru").C("links").Find(bson.M{"hash": id}).One(&dbData)
+// 	if c != nil {
+// 		// ctx.Redirect("/")
+// 	}
+// 	// ctx.Redirect(dbData.Link)
+// }
