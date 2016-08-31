d693b4fb1471c46509f24248b15f1dae1941fcbd
Arthur Silva <arthur.001.002@gmail.com>
2016-08-31 14:38:34 -0300

    added home test

--- main_test.go
+++ main_test.go
@@ -1,7 +1,25 @@
 package main
 
-func requestTest() {
-	req, _ := httptesting.NewRequest("GET", "/", nil)
-	w := httptesting.NewRecorder()
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHome(t *testing.T) {
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
 	Home(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Home page didn't return %v", http.StatusOK)
+	}
 }
+
+// func TestAddLink(t *testing.T) {
+// 	req := httptest.NewRequest("POST", "/", nil)
+// 	w := httptest.NewRecorder()
+// 	AddLink(w, req)
+// 	if w.Code != http.StatusNotFound {
+// 		t.Errorf("Home page didn't return %v", http.StatusOK)
+// 	}
+// }
